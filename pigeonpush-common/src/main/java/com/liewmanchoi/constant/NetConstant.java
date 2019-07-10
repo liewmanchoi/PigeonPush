a6a125668f573351cb6126f9d9aea4e986162476
Liew Manchoi <liewmanchoi@gmail.com>
2019-07-10 09:28:21 +0800

    feat(sdk): ConnectionListener
    重连逻辑
    
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

--- pigeonpush-common/src/main/java/com/liewmanchoi/constant/NetConstant.java
+++ pigeonpush-common/src/main/java/com/liewmanchoi/constant/NetConstant.java
@@ -5,8 +5,8 @@ package com.liewmanchoi.constant;
  * @date 2019/7/9
  */
 public class NetConstant {
-  public static final int HEARTBEAT_TIMEOUT = 20;
+  public static final int HEARTBEAT_TIMEOUT = 5;
   public static final int HEARTBEAT_TIMEOUT_MAX_TIMES = 3;
   public static final int RECONNECT_TIMEOUT = 10;
-  public static final int REAUTHENTICATE_TIMEOUT = 30;
+  public static final int REAUTHENTICATE_TIMEOUT = 10;
 }
