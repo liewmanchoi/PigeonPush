d5424ae6fc7414d334c2e67eafc5f8d9bbe9369d
Liew Manchoi <liewmanchoi@gmail.com>
2019-07-14 22:41:28 +0800

    feat(route.zookeeper): ZookeeperRegistry
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

---
+++ pigeonpush-common/src/main/java/com/liewmanchoi/constant/ZookeeperConstant.java
@@ -0,0 +1,10 @@
+package com.liewmanchoi.constant;
+
+/**
+ * @author wangsheng
+ * @date 2019/7/14
+ */
+public class ZookeeperConstant {
+
+  public static final String REG_PATH = "/pigeon/push-server";
+}
