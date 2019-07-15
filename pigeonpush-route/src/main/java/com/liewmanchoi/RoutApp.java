d11fa60307a467d7d89328cd26068e3726759888
Liew Manchoi <liewmanchoi@gmail.com>
2019-07-15 20:02:24 +0800

    feat(route.main): RouteApp
    启动窗口
    
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

---
+++ pigeonpush-route/src/main/java/com/liewmanchoi/RoutApp.java
@@ -0,0 +1,19 @@
+package com.liewmanchoi;
+
+import com.liewmanchoi.config.ZookeeperConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+/**
+ * @author wangsheng
+ * @date 2019/7/15
+ */
+@SpringBootApplication
+@EnableConfigurationProperties(ZookeeperConfig.class)
+public class RoutApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringApplication.class);
+  }
+}
