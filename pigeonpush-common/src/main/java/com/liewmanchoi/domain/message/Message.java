4a035c6241d4cfe628f901f22f8d30b6949bf8c1
Liew Manchoi <liewmanchoi@gmail.com>
2019-07-22 15:30:54 +0800

    feat(common.domain): Message
    修改Message中PUSH类型消息的构造
    
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

--- pigeonpush-common/src/main/java/com/liewmanchoi/domain/message/Message.java
+++ pigeonpush-common/src/main/java/com/liewmanchoi/domain/message/Message.java
@@ -23,9 +23,7 @@ public class Message implements Serializable {
   public static final byte AUTH_REQ = 1 << 4;
   public static final byte AUTH_RES = 1 << 5;
   public final transient Recycler.Handle<Message> handle;
-  // TODO: 合并PushMessage至扩展字段
-  /** 推送消息主体部分 */
-  PushMessage pushMessage;
+
   /** 消息类型，一共分为PUSH/ACK/PING/PONG/AUTH五种 */
   private byte type;
 
@@ -57,12 +55,34 @@ public class Message implements Serializable {
     return message;
   }
 
-  public static Message buildPush(String clientId, PushMessage pushMessage) {
+  public static Message buildPush(PushMessage pushMessage) {
     Message message = GlobalRecycler.reuse(Message.class);
     message.setType(Message.PUSH);
+    final String clientId = pushMessage.getClientId();
+
     message.setClientId(clientId);
-    message.setPushMessage(pushMessage);
+
+    Map<String, Object> attachment = new HashMap<>(3);
+    attachment.put("messageId", pushMessage.getMessageId());
+    attachment.put("title", pushMessage.getTitle());
+    attachment.put("text", pushMessage.getText());
+    message.setAttachment(attachment);
+
     return message;
+  }
+
+  public PushMessage getPushMessage() {
+    if (type != PUSH) {
+      return null;
+    }
+
+    PushMessage pushMessage = new PushMessage();
+    pushMessage.setClientId(clientId);
+    pushMessage.setMessageId((Integer) attachment.get("messageId"));
+    pushMessage.setTitle((String) attachment.get("title"));
+    pushMessage.setText((String) attachment.get("text"));
+
+    return pushMessage;
   }
 
   public Integer getPushMessageID() {
@@ -70,7 +90,7 @@ public class Message implements Serializable {
       return null;
     }
 
-    return
+    return (Integer) attachment.get("messageId");
   }
 
   public static Message buildACK(String clientId, int messageId) {
@@ -141,7 +161,6 @@ public class Message implements Serializable {
     this.type = 0;
     this.clientId = null;
     this.attachment = null;
-    this.pushMessage = null;
     handle.recycle(this);
   }
 }
