f6a9eca87af832d0596e6278b44a702c4f3435a3
Liew Manchoi <liewmanchoi@gmail.com>
2019-07-30 09:59:52 +0800

    fix(sdk): SDKUtil
    虚拟机中无法获取WIFI网卡mac地址，因此改为使用ip地址的16进制字符串作为唯一标识
    
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

--- pigeonpush-sdk/src/main/java/com/liewmanchoi/util/SDKUtil.java
+++ pigeonpush-sdk/src/main/java/com/liewmanchoi/util/SDKUtil.java
@@ -1,10 +1,5 @@
 package com.liewmanchoi.util;
 
-import com.liewmanchoi.exception.ClientException;
-import com.liewmanchoi.exception.ClientException.ErrorEnum;
-import java.net.NetworkInterface;
-import java.util.Collections;
-import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -13,49 +8,26 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class SDKUtil {
-  private static String DEVICE_ID = null;
+  private static String CLIENT_ID = null;
 
   /**
    * 获取设备唯一标识码
    *
    * @return deviceId，获取失败返回null
    */
-  public static String getDeviceId() {
-    // 为了简单起见，直接使用wifi网卡mac地址作为设备的唯一标识符
-    if (DEVICE_ID != null) {
-      return DEVICE_ID;
+  public static String getClientId() {
+    // 为了简单起见，直接使用ip地址作为设备的唯一标识符
+    if (CLIENT_ID != null) {
+      return CLIENT_ID;
     }
 
-    try {
-      List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
-      for (NetworkInterface nif : all) {
-        if (!"wlan0".equalsIgnoreCase(nif.getName())) {
-          continue;
-        }
-
-        byte[] macBytes = nif.getHardwareAddress();
-        if (macBytes == null) {
-          return null;
-        }
-
-        StringBuilder res = new StringBuilder();
-        for (byte b : macBytes) {
-          res.append(String.format("%02X:", b));
-        }
-
-        if (res.length() > 1) {
-          res.deleteCharAt(res.length() - 1);
-        }
-
-        DEVICE_ID = res.toString();
-        return DEVICE_ID;
-      }
-    } catch (Exception ex) {
-      log.error("获取WIFi网卡mac地址失败", ex);
-      throw new ClientException(ErrorEnum.DEVICE_ID_FAILURE, "获取deviceId失败");
+    byte[] ipBytes = IpUtil.getLocalAddress().getAddress();
+    StringBuilder stringBuilder = new StringBuilder();
+    for (byte b : ipBytes) {
+      stringBuilder.append(String.format("%02X", b));
     }
 
-    log.error("设备中不存在WIFI网卡");
-    throw new ClientException(ErrorEnum.DEVICE_ID_FAILURE, "设备中不存在WIFI网卡");
+    CLIENT_ID = stringBuilder.toString();
+    return CLIENT_ID;
   }
 }
